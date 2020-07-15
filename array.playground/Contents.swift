2bd0240c60cd4b47783ae6dda10cd9268f5adfe3
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-15 11:37:03 -0500

    Add files via upload

---
+++ array.playground/Contents.swift
@@ -0,0 +1,7 @@
+var someInts = [Int]()
+
+someInts.append(10)
+
+var instruments: [String] = ["guitar", "recorder", "harmonica", "lap harp", "stylophone", "drum pads", "bongo", "ukelele", "violin", "banjo"]
+
+print("The instruments list contains \(instruments.count) items.")
