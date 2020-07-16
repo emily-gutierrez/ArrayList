620adf36d9e6781b1cb5cacd32c40a111bd67365
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-16 15:20:54 -0500

    Add files via upload

--- array.playground/Contents.swift
+++ array.playground/Contents.swift
@@ -2,6 +2,13 @@ var someInts = [Int]()
 
 someInts.append(10)
 
-var instruments: [String] = ["guitar", "recorder", "harmonica", "lap harp", "stylophone", "drum pads", "bongo", "ukelele", "violin", "banjo"]
+var instrumentsList: [String] = ["guitar", "recorder", "harmonica", "lap harp", "stylophone", "drum pads", "bongo", "ukelele", "violin", "banjo"]
 
-print("The instruments list contains \(instruments.count) items.")
+print("The instruments list contains \(instrumentsList.count) items.")
+
+for item in instrumentsList {
+    print(item)
+}
+let companyArray = ["banjo", "bongo", "drum pads", "guitar", "harmonica", "lap harp", "recorder", "stylophone", "ukelele", "violin"]
+let sortedArray = companyArray.sorted(by: { $0 < $1})
+print(sortedArray)
